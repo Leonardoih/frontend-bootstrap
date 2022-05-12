b647c9db3e9a4c77161ddaea3ada3b15c6b692d7
leonardoih <leonardoih090217@gamil.com>
2022-05-11 21:43:55 -0500

    nuevo

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,54 @@
+import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export function Register() {
-	const { user } = useAuth();
-	console.log(user);
-	return <div>Register</div>;
+	const { signup } = useAuth();
+
+	const [user, setUser] = useState({
+		email: '',
+		password: '',
+	});
+
+	const navigate = useNavigate();
+	const [error, setError] = useState();
+
+	const hadleCahnge = ({ target: { name, value } }) => {
+		setUser({ ...user, [name]: value });
+	};
+
+	const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signup(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+	return (
+		<div>
+			{error && <p>{error}</p>}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='email'>Email</label>
+				<input
+					type='email'
+					name='email'
+					placeholder='example@mail.com'
+					onChange={hadleCahnge}
+				/>
+
+				<label htmlFor='password'>Password</label>
+				<input
+					type='password'
+					name='password'
+					placeholder='password'
+					onChange={hadleCahnge}
+				/>
+				<button>Register</button>
+			</form>
+		</div>
+	);
 }
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,20 +1,22 @@
 import { createContext, useContext } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export const authContext = createContext();
 
 export const useAuth = () => {
 	const context = useContext(authContext);
-  if (!context) throw new Error('useAuth must be used within an AuthProvider');
-  return context;
+	if (!context)
+		throw new Error('useAuth must be used within an AuthProvider');
+	return context;
 };
 
-
 export function AuthProvider({ children }) {
-	const user = {
-		login: true,
+	const signup = (email, password) => {
+		createUserWithEmailAndPassword(auth, email, password);
 	};
 	return (
-		<authContext.Provider value={{ user }}>
+		<authContext.Provider value={{ signup }}>
 			{children}
 		</authContext.Provider>
 	);
